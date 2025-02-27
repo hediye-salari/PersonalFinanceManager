5bce13f86b19ef87a9254991d8ccaf2ada2431a4
hp <Salari.hediye@gmail.com>
2025-02-27 14:04:13 +0330

    Create CategoryService.Models

---
+++ CategoryService/Models/Category .cs
@@ -0,0 +1,11 @@
+﻿namespace CategoryService.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+    }
+}
+
