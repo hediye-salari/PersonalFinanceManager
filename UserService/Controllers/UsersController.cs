c8bbff3492aa12195cb7a8299c161a99e6a5fc59
hp <Salari.hediye@gmail.com>
2025-02-27 16:56:49 +0330

    Add Model To Controller

---
+++ UserService/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserService.Models;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly List<User> _users = new();
+        //ثبت نام کاربر (Register)
+        [HttpPost("register")]
+        public IActionResult Register(User user)
+        {
+            _users.Add(user);
+            return Ok();
+        }
+        //ورود کاربر (Login)
+        [HttpPost("login")]
+        public IActionResult Login(User user)
+        {
+            var existingUser = _users.FirstOrDefault(u => u.Username == user.Username);
+            if (existingUser == null || existingUser.PasswordHash != user.PasswordHash)
+                return Unauthorized();
+
+            return Ok();
+        }
+    }
+}
