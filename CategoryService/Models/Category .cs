42793ad4be2fe10d5c0df5287671d90d68188604
hp <Salari.hediye@gmail.com>
2025-02-27 14:12:59 +0330

    Alter Modes for required property and nullabel propert

--- CategoryService/Models/Category .cs
+++ CategoryService/Models/Category .cs
@@ -3,9 +3,9 @@
     public class Category
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-        public string Email { get; set; }
+        public required string Username { get; set; }
+        public required string PasswordHash { get; set; }
+        public string? Email { get; set; }
     }
 }
 
--- TransactionService/Models/Transaction.cs
+++ TransactionService/Models/Transaction.cs
@@ -3,8 +3,8 @@
     public class Transaction
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-        public string Email { get; set; }
+        public required string Username { get; set; }
+        public required string PasswordHash { get; set; }
+        public string? Email { get; set; }
     }
 }
